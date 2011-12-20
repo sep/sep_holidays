fa501c0046c51d219c940bbd8dbbe34dabd21a27
Matt Swanson <mdswanson@sep.com>
2011-12-19 22:20:57 -0500

    fix inlined comments...

--- lib/sep_holidays.rb
+++ lib/sep_holidays.rb
@@ -2,24 +2,24 @@ require 'date'
 
 class Date
     SEP_HOLIDAYS = [
-        Date.new(2010, 12, 31), #New Year's Day
-        Date.new(2011, 5, 30),  #Memorial Day
-        Date.new(2011, 7, 4),   #Independence Day
-        Date.new(2011, 9, 5),   #Labor Day
-        Date.new(2011, 11, 24), #Thanksgiving (2 days)
+        Date.new(2010, 12, 31),
+        Date.new(2011, 5, 30),
+        Date.new(2011, 7, 4),
+        Date.new(2011, 9, 5),
+        Date.new(2011, 11, 24),
         Date.new(2011, 11, 25),
-        Date.new(2011, 12, 26), #Christmas (5 days)
+        Date.new(2011, 12, 26),
         Date.new(2011, 12, 27),
         Date.new(2011, 12, 28),
         Date.new(2011, 12, 29),
         Date.new(2011, 12, 30),
-        Date.new(2012, 1, 2),   #New Year's Day
-        Date.new(2012, 5, 28),  #Memorial Day
-        Date.new(2012, 7, 4),   #Independence Day
-        Date.new(2012, 9, 3),   #Labor Day
-        Date.new(2012, 11, 22), #Thanksgiving (2 days)
+        Date.new(2012, 1, 2),
+        Date.new(2012, 5, 28),
+        Date.new(2012, 7, 4),
+        Date.new(2012, 9, 3),
+        Date.new(2012, 11, 22),
         Date.new(2012, 11, 23),
-        Date.new(2012, 12, 24), #Christmas (5 days)
+        Date.new(2012, 12, 24),
         Date.new(2012, 12, 25),
         Date.new(2012, 12, 26),
         Date.new(2012, 12, 27),
